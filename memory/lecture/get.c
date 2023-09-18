775470083fb663eef40302c2cb01b4989e06cab2
Eduardo Lustosa <124291493+edulustosa@users.noreply.github.com>
2023-09-18 18:32:48 -0300

    End of memory lecture

---
+++ memory/lecture/get.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    /*
+        int x;
+        printf("x: ");
+        scanf("%d", &x);
+        printf("x: %d", x);
+    */
+
+   char s[4];
+   printf("s: ");
+   scanf("%s", s);
+   printf("s: %s\n", s);
+    
+    return 0;
+}

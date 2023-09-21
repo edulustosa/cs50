5223f63fd70543d1c493e8525582beb0c51860c2
Eduardo Lustosa <124291493+edulustosa@users.noreply.github.com>
2023-09-21 15:36:50 -0300

    End of lecture

--- data-structures/lecture/list.c
+++ data-structures/lecture/list.c
@@ -1,37 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
+typedef struct node
 {
-    int *list = malloc(3 * sizeof(int));
-    if (list == NULL)
+    int num;
+    struct node *next;
+} node;
+
+int main(int argc, char *argv[])
+{
+    node *list = NULL;
+
+    for (int i = 1; i < argc; i++)
     {
-        return 1;
+        int number = atoi(argv[i]);
+
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            return 1;
+        }
+
+        n->num = number;
+        n->next = NULL;
+
+        n->next = list;
+        list = n;
     }
 
-    list[0] = 1;
-    list[1] = 2;
-    list[2] = 3;
-
-    // ...
-
-    int *tmp = realloc(list, 4 * sizeof(int));
-    if (tmp == NULL)
+    node *ptr = list;
+    while (ptr != NULL)
     {
-        free(list);
-        return 1;
+        printf("%d ", ptr->num);
+        ptr = ptr->next;
     }
 
-    list = tmp;
-
-    list[3] = 4;
-
-    for (int i = 0; i < 4; i++)
+    ptr = list;
+    while (ptr != NULL)
     {
-        printf("%d ", list[i]);
+        node *next = ptr->next;
+        free(ptr);
+        ptr = next;
     }
-
-    free(list);
 
     return 0;
 }

c526c0ce55a6aa3c9c605aba9f9c713f3670a464
Eduardo Lustosa <124291493+edulustosa@users.noreply.github.com>
2023-10-02 15:18:33 -0300

    End of lecture

--- sql/lecture/favorites.py
+++ sql/lecture/favorites.py
@@ -1,18 +1,34 @@
-import csv
+import sqlite3
 
-with open("favorites.csv") as file:
-    reader = csv.DictReader(file)
-    counts = {}
-    for line in reader:
-        favorite = line["problem"]
-        if favorite in counts:
-            counts[favorite] += 1
-        else:
-            counts[favorite] = 1
+conn = sqlite3.connect("favorites.db")
+cursor = conn.cursor()
 
 favorite = input("Favorite: ")
-if favorite in counts:
-    print(f"{favorite}: {counts[favorite]}")
 
-# for favorite in sorted(counts, key=lambda problem: counts[problem], reverse=True):
+cursor.execute("SELECT COUNT(*) FROM favorites WHERE problem = ?", (favorite,))
+results = cursor.fetchall()
+
+for line in results:
+    print(line[0])
+
+conn.close()
+
+# import csv
+# import sqlite3
+
+# with open("favorites.csv") as file:
+#     reader = csv.DictReader(file)
+#     counts = {}
+#     for line in reader:
+#         favorite = line["problem"]
+#         if favorite in counts:
+#             counts[favorite] += 1
+#         else:
+#             counts[favorite] = 1
+
+# favorite = input("Favorite: ")
+# if favorite in counts:
 #     print(f"{favorite}: {counts[favorite]}")
+
+# # for favorite in sorted(counts, key=lambda problem: counts[problem], reverse=True):
+# #     print(f"{favorite}: {counts[favorite]}")
